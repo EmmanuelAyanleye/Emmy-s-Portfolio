165f3cc91b86cacb081f9d27fb622dd107a74cc0
EmmanuelAyanleye <ayanleyeemmanuel5@gmail.com>
2025-05-02 02:09:51 +0100

    Form page

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib import messages
+from .forms import ContactForm
 
 def home(request):
     template = loader.get_template("index.html")
     return HttpResponse(template.render())
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your message has been sent successfully!')
+            return redirect('contact')
+    else:
+        form = ContactForm()
+    
+    return render(request, 'contact.html', {'form': form})
